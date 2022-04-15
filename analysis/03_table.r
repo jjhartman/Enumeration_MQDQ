19f0c97090b70381034dd1496f6740df169c6cd8
Joshua Hartman <joshuahartman@c02x40yrjgh5.lan>
2022-04-15 12:11:32 -0400

    Update table command and corresponding tables

--- analysis/03_table.r
+++ analysis/03_table.r
@@ -4,15 +4,17 @@ library(kableExtra)
 #---------------------------------
 # no exclusions, enumerativeness == 1
 #---------------------------------
-author_table <- function(df){
+author_table <- function(df, note =  ""){
+  save_to <- file.path("tables", paste0(unique(df$folder), note, "_authortable.html"))
+  message("Saving to ", save_to)
   
   df %>%
-    select(author_name, date, enumerativeness_rate1, enumerativeness_rate8, n, one_count, high_count, note = "") %>% 
+    select(author_name, date, enumerativeness_rate1, enumerativeness_rate8, n, one_count, high_count) %>% 
     unique() %>%
     arrange(desc(enumerativeness_rate1)) %>% 
     kable(format = "html") %>%
     kable_styling(font_size = 10) %>%
-    save_kable(file = file.path("tables", paste0(unique(df$folder), note, "_authortable.html")), self_contained = T)
+    save_kable(file = save_to, self_contained = T)
   # You can open this html file in Word, and it will be an editable table there. 
 }
 
